5c427ffc0977fc9b499730b6026201d5f8df0bee
Anders Busch <andersbusch@gmail.com>
2020-09-01 17:14:57 +0200

    breaking percentage is now a variable in the position component

--- game/components/positionalComponent.js
+++ game/components/positionalComponent.js
@@ -1,8 +1,9 @@
 import Vector from '../dataStructures/vector.js'
 
 export default class PositionComponent {
-    constructor(position, velocity) {
-        this.position = position || new Vector(0, 0);
-        this.velocity = velocity || new Vector(0, 0);
-    }
+  constructor(position, velocity, breakingForcePercentage) {
+    this.position = position || new Vector(0, 0);
+    this.velocity = velocity || new Vector(0, 0);
+    this.breakingForcePercentage = breakingForcePercentage || 0.6;
+  }
 }
--- game/systems/physicsSystem.js
+++ game/systems/physicsSystem.js
@@ -1,18 +1,18 @@
-import entityManager from "../dataStructures/entityManager";
-import PositionComponent from "../components/positionalComponent";
+import PositionComponent from '../components/positionalComponent';
+import entityManager from '../dataStructures/entityManager';
+import Variables from '../sharedVariables.js';
 
 export default class PhysicsSystem {
+  update() {
+    let entities = entityManager.getEntitiesByComponents(PositionComponent);
 
-    update() {
-        let entities = entityManager.getEntitiesByComponents(
-            PositionComponent
-        );
+    for (let entity of entities) {
+      let [positionComp] = entity;
 
-        for (let entity of entities) {
-            let [positionComp] = entity;
-            positionComp.velocity.mulMut(0.9)
-            
-            positionComp.position.addMut(positionComp.velocity);
-        }
+      positionComp.velocity.mulMut(1 - positionComp.breakingForcePercentage)
+
+      positionComp.position.x += positionComp.velocity.x;
+      positionComp.position.y += positionComp.velocity.y;
     }
+  }
 };
