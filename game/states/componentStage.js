63fc2ee438d83a4d9e1ab55b0d85002ecd9b1579
Anders Busch <andersbusch@gmail.com>
2020-09-01 17:15:58 +0200

    componentStage is now refactored to only two entities

--- game/states/componentStage.js
+++ game/states/componentStage.js
@@ -1,60 +1,51 @@
-import GameState from '../dataStructures/gameState.js'
-import entityManager from '../dataStructures/entityManager.js'
-import PositionalComponent from '../components/positionalComponent.js'
+import ControllableComponent from '../components/controllableComponent.js';
 import DimensionalComponent from '../components/dimensionalComponent.js';
 import DrawableComponent from '../components/drawableComponent.js';
+import PositionalComponent from '../components/positionalComponent.js'
+import entityManager from '../dataStructures/entityManager.js'
+import GameState from '../dataStructures/gameState.js'
 import Vector from '../dataStructures/vector.js';
-import DrawingSystem from '../systems/drawingSystem.js';
-import ControllableComponent from '../components/controllableComponent.js';
-import ControlSystem from '../systems/controlSystem.js';
 import sharedConstants from '../sharedConstants.js';
+import ControlSystem from '../systems/controlSystem.js';
+import DrawingSystem from '../systems/drawingSystem.js';
+import PathFollowingSystem from '../systems/pathFollowingSystem.js';
 import PhysicsSystem from '../systems/physicsSystem.js';
+import PathComponent from '../components/pathComponent.js';
+import CircularBuffer from '../dataStructures/circularBuffer.js';
 
 export default class ComponentStage extends GameState {
+  load() {
+    entityManager.createNewEntity(
+        new PositionalComponent(new Vector(
+            ((sharedConstants.CANVAS_WIDTH / 2) - 32 / 2),
+            (sharedConstants.CANVAS_HEIGHT -
+             (sharedConstants.CANVAS_HEIGHT / 8) - 32))),
+        new DimensionalComponent(new Vector(32, 32)),
+        new DrawableComponent('blue'),
+        new ControllableComponent(0, new Vector(10, 10)));
 
-    load() {
-        entityManager.createNewEntity(
-            new PositionalComponent(),
-            new DimensionalComponent(new Vector(32, 32)),
-            new DrawableComponent(),
-            new ControllableComponent(0, new Vector(5, 0))
-        );
+    entityManager.createNewEntity(
+        new PositionalComponent(new Vector(
+            (sharedConstants.CANVAS_WIDTH - 32),
+            (sharedConstants.CANVAS_HEIGHT / 10)),
+            new Vector(5, 5)),
+        new DimensionalComponent(new Vector(32, 32)),
+        new DrawableComponent('red'),
+        new PathComponent(new CircularBuffer(
+          new Vector((sharedConstants.CANVAS_WIDTH - 32) - 64, (sharedConstants.CANVAS_HEIGHT / 10)),
+          new Vector(0, (sharedConstants.CANVAS_HEIGHT / 10))
+        )));
 
-        entityManager.createNewEntity(
-            new PositionalComponent(
-                new Vector( sharedConstants.CANVAS_WIDTH - 32, sharedConstants.CANVAS_HEIGHT - 32)),
-            new DimensionalComponent(new Vector(32, 32)),
-            new DrawableComponent('red'),
-            new ControllableComponent(0, new Vector(5, 0))
-        );
+    this.systems = [
+      new PathFollowingSystem(), new ControlSystem(), new PhysicsSystem(),
+      new DrawingSystem()
+    ]
+    super.load();
+  }
 
-        entityManager.createNewEntity(
-            new PositionalComponent(
-                new Vector( 0, sharedConstants.CANVAS_HEIGHT - 32)),
-            new DimensionalComponent(new Vector(32, 32)),
-            new DrawableComponent('green'),
-            new ControllableComponent(0, new Vector(5, 0))
-        );
-
-        entityManager.createNewEntity(
-            new PositionalComponent(
-                new Vector( sharedConstants.CANVAS_WIDTH - 32, 0)),
-            new DimensionalComponent(new Vector(32, 32)),
-            new DrawableComponent('violet'),
-            new ControllableComponent(0, new Vector(5, 0))
-        );
-
-        this.systems = [
-            new ControlSystem(),
-            new PhysicsSystem(),
-            new DrawingSystem()
-        ]
-        super.load();
+  update() {
+    for (let system of this.systems) {
+      system.update();
     }
-
-    update() {
-        for (let system of this.systems) {
-            system.update();
-        }
-    }
+  }
 };
