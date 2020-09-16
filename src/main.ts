36f27c1e3350b1cd116813439ed43b27292543af
Anders Busch <andersbusch@gmail.com>
2020-09-16 20:47:56 +0200

    pausing of state when unfocused

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,27 @@ import Constants from './SharedConstants';
 import Variables from './SharedVariables';
 import ComponentStage from './states/ComponentState';
 
+Variables.canvasManager.setup();
 
 new LevelLoader().loadFromJson('levels/first.level.json').then(() => {
-  Variables.canvasManager.setup();
 
   Variables.stateStack.pushState(new ComponentStage());
 
+  window.onblur = () => {
+    Variables.isPaused = true;
+  }
+
+  window.onfocus = () => {
+    Variables.isPaused = false;
+  }
+
   const gameLoop = () => {
+
+    if (Variables.isPaused) {
+      window.requestAnimationFrame(gameLoop);
+      return;
+    }
+
     Variables.frameClock.update();
 
     Variables.canvasManager.getCanvasContext().clearRect(
